27ab4ac3400ca6bc984c91117569fa53408565a3
blance714 <blance_714@outlook.com>
2026-04-04 22:36:07 +0800

    Initial Commit

---
+++ KairosTests/KairosTests.swift
@@ -0,0 +1,18 @@
+//
+//  KairosTests.swift
+//  KairosTests
+//
+//  Created by blance on 04/04/2026.
+//
+
+import Testing
+
+struct KairosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
